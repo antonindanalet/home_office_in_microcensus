5b8b2f8d0d8ffd5830aac10b8c4652f43e46e7da
antonindanalet <antonin.danalet>
2020-10-23 19:37:34 +0200

    Added the encoding, so that it works on mac

--- src/utils_mtmc/get_mtmc_files.py
+++ src/utils_mtmc/get_mtmc_files.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def get_zp(year, selected_columns=None):
     if year == 2015:
         folder_path = Path('../data/input/mtmc/2015/')
-        with open(folder_path / 'zielpersonen.csv', 'r') as zielpersonen_file:
+        with open(folder_path / 'zielpersonen.csv', 'r', encoding='latin1') as zielpersonen_file:
             if selected_columns is None:
                 df_zp = pd.read_csv(zielpersonen_file)
             else:
@@ -20,7 +20,7 @@ def get_zp(year, selected_columns=None):
 def get_hh(year, selected_columns=None):
     if year == 2015:
         folder_path_2015 = Path('../data/input/mtmc/2015/')
-        with open(folder_path_2015 / 'haushalte.csv', 'r') as haushalte_file:
+        with open(folder_path_2015 / 'haushalte.csv', 'r', encoding='latin1') as haushalte_file:
             df_hh = pd.read_csv(haushalte_file,
                                 dtype={'HHNR': int},
                                 usecols=selected_columns)
