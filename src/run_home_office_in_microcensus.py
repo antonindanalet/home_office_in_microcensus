1064efbaf81bd38c2f34b2a9a2788d058f69a5cd
antonindanalet <antonin.danalet>
2020-11-20 09:50:01 +0100

    New structure of the code, separating internal validation and application to synpop

--- src/run_home_office_in_microcensus.py
+++ src/run_home_office_in_microcensus.py
@@ -1,10 +1,12 @@
 from estimate_choice_model_home_office import estimate_choice_model_home_office
 from validate_choice_model_home_office import validate_choice_model_home_office
+from apply_model_to_synthetic_population import apply_model_to_synthetic_population
 
 
 def run_home_office_in_microcensus():
     estimate_choice_model_home_office()
     validate_choice_model_home_office()
+    apply_model_to_synthetic_population()
 
 
 if __name__ == '__main__':
