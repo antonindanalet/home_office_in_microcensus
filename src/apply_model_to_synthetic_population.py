e5ee4c0a8b3c0f9df2896de0c083856b86077922
antonindanalet <antonin.danalet>
2020-12-12 17:57:14 +0100

    Application of the model to the synpop, including public transport accessibility of work place

--- src/apply_model_to_synthetic_population.py
+++ src/apply_model_to_synthetic_population.py
@@ -4,7 +4,7 @@ import biogeme.database as db
 from biogeme.expressions import Beta
 import biogeme.models as models
 import biogeme.biogeme as bio
-import biogeme.results as res
+import biogeme.messaging as msg
 import os
 from utils_synpop.generate_data_file_for_simulation import generate_data_file_for_simulation
 from utils_mtmc.get_mtmc_files import get_zp
@@ -16,11 +16,14 @@ def apply_model_to_synthetic_population(betas):
     ''' External validation using a synthetic population '''
     # Prepare the data for PandasBiogeme
     generate_data_file_for_simulation()
+    # Definition of the household income limit corresponding to the distribution of the MTMC
+    household_income_limit = compute_household_income_limit()
     # Simulate the model on the synthetic population
     data_file_directory_for_simulation = Path('../data/output/data/validation_with_SynPop/')
     data_file_name_for_simulation = 'persons_from_SynPop2017.csv'
     output_directory_for_simulation = Path('../data/output/models/validation_with_SynPop/')
-    run_simulation(data_file_directory_for_simulation, data_file_name_for_simulation, output_directory_for_simulation)
+    run_simulation(data_file_directory_for_simulation, data_file_name_for_simulation, output_directory_for_simulation,
+                   betas, household_income_limit)
     # Compare rate of home office in the MTMC and in the synthetic population
     descr_stat_mtmc()
     descr_stat_synpop()
@@ -31,7 +34,8 @@ def descr_stat_synpop():
     synpop_filename = 'persons_from_SynPop_with_probability_home_office.csv'
     # Get the data
     df_persons = pd.read_csv(synpop_directory / synpop_filename, sep=',')
-    print(df_persons.loc[df_persons.employed == 1, 'Prob. home office'].describe())
+    df_persons.drop(df_persons[df_persons.position_in_bus.isin([-99, 0, 3])].index, inplace=True)
+    print('Proportion of home office (synpop):', df_persons['Prob. home office'].mean())
 
 
 def descr_stat_mtmc():
@@ -54,7 +58,8 @@ def descr_stat_mtmc():
           str(weighted_avg * 100) + '% (+/-', str(weighted_std * 100) + ', n=' + str(nb_obs) + ')')
 
 
-def run_simulation(data_file_directory_for_simulation, data_file_name_for_simulation, output_directory_for_simulation):
+def run_simulation(data_file_directory_for_simulation, data_file_name_for_simulation, output_directory_for_simulation,
+                   betas, household_income_limit):
     """
         :author: Antonin Danalet, based on the example '01logit_simul.py' by Michel Bierlaire, EPFL, on biogeme.epfl.ch
 
@@ -77,22 +82,13 @@ def run_simulation(data_file_directory_for_simulation, data_file_name_for_simula
 
     b_male = Beta('b_male', 0, None, None, 0)
 
-    b_single_household = Beta('b_single_household', 0, None, None, 1)
-    b_couple_without_children = Beta('b_couple_without_children', 0, None, None, 1)
-    b_couple_with_children = Beta('b_couple_with_children', 0, None, None, 1)
-    b_single_parent_with_children = Beta('b_single_parent_with_children', 0, None, None, 1)
-    b_not_family_household = Beta('b_not_family_household', 0, None, None, 1)
+    b_public_transport_connection_quality_are_na_home = Beta('b_public_transport_connection_quality_are_na_home',
+                                                             0, None, None, 0)
 
-    b_public_transport_connection_quality_are_a = Beta('b_public_transport_connection_quality_are_a', 0, None, None, 1)
-    b_public_transport_connection_quality_are_b = Beta('b_public_transport_connection_quality_are_b', 0, None, None, 1)
-    b_public_transport_connection_quality_are_c = Beta('b_public_transport_connection_quality_are_c', 0, None, None, 1)
-    b_public_transport_connection_quality_are_d = Beta('b_public_transport_connection_quality_are_d', 0, None, None, 1)
-    b_public_transport_connection_quality_are_na = Beta('b_public_transport_connection_quality_are_na', 0, None, None,
-                                                        0)
+    b_public_transport_connection_quality_are_a_work = Beta('b_public_transport_connection_quality_are_a_work',
+                                                            0, None, None, 0)
 
-    b_urban = Beta('b_urban', 0, None, None, 1)
-    b_rural = Beta('b_rural', 0, None, None, 1)
-    b_intermediate = Beta('b_intermediate', 0, None, None, 1)
+    b_rural_work = Beta('b_rural_work', 0, None, None, 0)
 
     b_home_work_distance = Beta('b_home_work_distance', 0, None, None, 0)
 
@@ -109,12 +105,7 @@ def run_simulation(data_file_directory_for_simulation, data_file_name_for_simula
     b_executives = Beta('b_executives', 0, None, None, 0)
     b_german = Beta('b_german', 0, None, None, 0)
     b_nationality_ch_germany_france_italy_nw_e = Beta('b_nationality_ch_germany_france_italy_nw_e', 0, None, None, 0)
-    # b_nationality_south_west_europe = Beta('b_nationality_south_west_europe', 0, None, None, 1)
-    # b_nationality_southeast_europe = Beta('b_nationality_southeast_europe', 0, None, None, 1)
-    # b_several_part_time_jobs = Beta('b_several_part_time_jobs', 0, None, None, 1)
-    # b_hh_income_na = Beta('B_hh_income_na', 0, None, None, 1)
     b_hh_income_8000_or_less = Beta('b_hh_income_8000_or_less', 0, None, None, 0)
-    # b_hh_income_more_than_8000 = Beta('b_hh_income_more_than_8000', 0, None, None, 1)
 
     # Definition of new variables
     no_post_school_educ = education == 1
@@ -124,7 +115,8 @@ def run_simulation(data_file_directory_for_simulation, data_file_name_for_simula
 
     male = (sex == 1)
 
-    public_transport_connection_quality_ARE_NA = (public_transport_connection_quality_ARE == 5)
+    public_transport_connection_quality_ARE_NA_home = (public_transport_connection_quality_ARE_home == 5)
+    public_transport_connection_quality_ARE_A_work = (public_transport_connection_quality_ARE_work == 1)
 
     home_work_distance = (home_work_crow_fly_distance * (home_work_crow_fly_distance >= 0.0) / 100000.0)
 
@@ -145,7 +137,8 @@ def run_simulation(data_file_directory_for_simulation, data_file_name_for_simula
     nationality_france_monaco_san_marino = nation == 3
     nationality_northwestern_europe = nation == 4
     nationality_eastern_europe = nation == 7
-    hh_income_8000_or_less = (hh_income == 1) + (hh_income == 2) + (hh_income == 3) + (hh_income == 4)
+    hh_income_8000_or_less = hh_income < household_income_limit
+    executives = (0 < position_in_bus) * (position_in_bus < 19)
 
     #  Utility
     U_home_office = alternative_specific_constant + \
@@ -155,7 +148,9 @@ def run_simulation(data_file_directory_for_simulation, data_file_name_for_simula
                     b_tertiary_education * tertiary_education + \
                     b_university * university + \
                     b_male * male + \
-                    b_public_transport_connection_quality_are_na * public_transport_connection_quality_ARE_NA + \
+                    b_public_transport_connection_quality_are_na_home * public_transport_connection_quality_ARE_NA_home + \
+                    b_public_transport_connection_quality_are_a_work * public_transport_connection_quality_ARE_A_work + \
+                    b_rural_work * rural_work + \
                     b_home_work_distance * home_work_distance + \
                     models.piecewiseFormula(age, [0, 20, 35, 75, 200]) + \
                     b_business_sector_agriculture * business_sector_agriculture + \
@@ -175,7 +170,7 @@ def run_simulation(data_file_directory_for_simulation, data_file_name_for_simula
                     b_nationality_ch_germany_france_italy_nw_e * nationality_france_monaco_san_marino + \
                     b_nationality_ch_germany_france_italy_nw_e * nationality_northwestern_europe + \
                     b_nationality_ch_germany_france_italy_nw_e * nationality_eastern_europe + \
-                    models.piecewiseFormula(work_percentage, [0, 90, 170]) + \
+                    models.piecewiseFormula(work_percentage, [0, 90, 101]) + \
                     b_hh_income_8000_or_less * hh_income_8000_or_less
     U_no_home_office = 0
 
@@ -196,12 +191,20 @@ def run_simulation(data_file_directory_for_simulation, data_file_name_for_simula
     # Create the Biogeme object
     biogeme = bio.BIOGEME(database, simulate)
     biogeme.modelName = 'logit_home_office_simul'
+
+    # Define level of verbosity
+    logger = msg.bioMessage()
+    # logger.setSilent()
+    logger.setWarning()
+    # logger.setGeneral()
+    # logger.setDetailed()
+
     # Get the betas from the estimation (without corrections)
     # path_to_estimation_folder = Path('../data/output/models/estimation/')
     # if os.path.isfile(path_to_estimation_folder / 'logit_home_office~00.pickle'):
     #     raise Exception('There are several model outputs! Careful.')
     # results = res.bioResults(pickleFile=path_to_estimation_folder / 'logit_home_office.pickle')
-    # betas = results.getBetaValues()
+    # betas_without_correction = results.getBetaValues()
 
     # Change the working directory, so that biogeme writes in the correct folder, i.e., where this file is
     standard_directory = os.getcwd()
@@ -219,8 +222,34 @@ def run_simulation(data_file_directory_for_simulation, data_file_name_for_simula
     df_persons.loc[df_persons.employed == 0, 'Prob. no home office'] = 1.0  # Unemployed people
     df_persons.loc[df_persons.employed == -99, 'Prob. home office'] = 0.0  # Other people
     df_persons.loc[df_persons.employed == -99, 'Prob. no home office'] = 1.0  # Other people
+    # By definition, apprentices don't do home office (because they were not asked in the MTMC)
+    df_persons.loc[df_persons.position_in_bus == 3, 0] = 1.0
+    df_persons.loc[df_persons.position_in_bus == 3, 1] = 0.0
+    df_persons.loc[df_persons.position_in_bus == 3, 2] = 0.0
+    df_persons.loc[df_persons.position_in_bus == 3, 3] = 0.0
+    df_persons.loc[df_persons.position_in_bus == 3, 4] = 0.0
 
     ''' Save the file '''
     output_directory = Path('../data/output/models/validation_with_SynPop/')
     data_file_name = 'persons_from_SynPop_with_probability_home_office.csv'
     df_persons.to_csv(output_directory / data_file_name, sep=',', index=False)
+
+
+def compute_household_income_limit():
+    # Proportion of employees with household income < 8000 CHF (MTMC):
+    percent_employees_with_income_less_8000 = 0.38921317978801245
+    # Read SynPop data
+    output_directory = Path('../data/output/data/validation_with_SynPop/')
+    data_file_name = 'persons_from_SynPop2017.csv'
+    df_persons_from_synpop = pd.read_csv(output_directory / data_file_name, sep=';')
+    # Remove children
+    df_persons_from_synpop.drop(df_persons_from_synpop[df_persons_from_synpop.age <= 5].index, inplace=True)
+    # Remove people unemployed (0) and apprentices (3)
+    df_persons_from_synpop.drop(df_persons_from_synpop[df_persons_from_synpop.position_in_bus.isin([-99, 0, 3])].index,
+                                inplace=True)
+    number_of_employees = len(df_persons_from_synpop)
+    number_of_employees_with_income_less_than_8000 = int(number_of_employees * percent_employees_with_income_less_8000)
+    df_persons_from_synpop_sorted = df_persons_from_synpop.sort_values(by='hh_income').reset_index()
+    household_income_limit = df_persons_from_synpop_sorted.loc[number_of_employees_with_income_less_than_8000,
+                                                               'hh_income']
+    return household_income_limit
--- src/utils_synpop/generate_data_file_for_simulation.py
+++ src/utils_synpop/generate_data_file_for_simulation.py
@@ -3,6 +3,7 @@ import pandas as pd
 from math import isnan
 import numpy as np
 # import shapely
+import matplotlib.pyplot as plt
 import geopandas
 
 
@@ -41,9 +42,20 @@ def add_information_about_businesses_from_synthetic_population(df_persons):
         df_businesses = pd.read_csv(businesses_file, sep=';', usecols=selected_columns)
     df_businesses.rename(columns={'xcoord': 'xcoord_work',
                                   'ycoord': 'ycoord_work'}, inplace=True)
+    ''' Add the urban/rural typology of the work place '''
+    # df_businesses = add_urban_rural_typology(df_businesses)
+    ''' Add the public transport connection quality of the work place '''
+    df_businesses = add_public_transport_connection_quality_work(df_businesses)
     df_persons = pd.merge(df_persons, df_businesses, on='business_id', how='left')  # Add the result to df_persons
+    df_persons['public_transport_connection_quality_ARE_work'].fillna('-99', inplace=True)  # Empty work places
     del df_persons['business_id']
     ''' Compute home-work distance: Add the distance (in meters) between home and work places '''
+    df_persons = add_home_work_distance(df_persons)
+    df_persons.drop(['xcoord_work', 'ycoord_work', 'xcoord_home', 'ycoord_home', 'geometry'], axis=1, inplace=True)
+    return df_persons
+
+
+def add_home_work_distance(df_persons):
     df_persons_with_work_coordinates = df_persons[df_persons.xcoord_work.notnull()]
     geodf_home = geopandas.GeoDataFrame(df_persons_with_work_coordinates,
                                         geometry=geopandas.points_from_xy(df_persons_with_work_coordinates.ycoord_home,
@@ -55,7 +67,7 @@ def add_information_about_businesses_from_synthetic_population(df_persons):
                                         crs='epsg:21781')
     df_persons.loc[df_persons.xcoord_work != -999, 'home_work_crow_fly_distance'] = geodf_home.distance(geodf_work)
     df_persons['home_work_crow_fly_distance'].fillna(-999, inplace=True)
-    df_persons.drop(['xcoord_work', 'ycoord_work', 'xcoord_home', 'ycoord_home', 'geometry'], axis=1, inplace=True)
+
     return df_persons
 
 
@@ -93,6 +105,8 @@ def get_persons_from_synthetic_population():
     with open(synpop_folder_path / 'persons.csv', 'r') as persons_file:
         df_persons = pd.read_csv(persons_file, sep=';', usecols=selected_columns)
     ''' Transform the data structure '''
+    # Replace NA values by -99 for position_in_bus
+    df_persons['position_in_bus'] = df_persons['position_in_bus'].fillna(-99)
     # Transform the 'date of birth' variable to an 'age' variable (reference year: 2017)
     df_persons['age'] = 2017 - df_persons['dbirth'].str.split('-', expand=True)[2].astype(int)
     del df_persons['dbirth']
@@ -100,28 +114,65 @@ def get_persons_from_synthetic_population():
     df_persons['employed'] = df_persons['position_in_bus'].apply(lambda x:
                                                                  -99 if isnan(x) else (0 if x == 0 else 1))
     ''' Add information about work '''
-    df_persons['executives'] = df_persons['position_in_bus'].apply(lambda x: 1 if (0 < x < 19) else 0)
-    del df_persons['position_in_bus']
     # Transform the variable about level of employment to a binary variable about working full time
     df_persons.rename(columns={'type_2': 'work_percentage'}, inplace=True)
     # Aggregate the income per person to get the income per household
     df_hh_income = df_persons[['household_id', 'income']].groupby(['household_id']).sum()  # Aggregate over people
     df_hh_income = df_hh_income.rename(columns={'income': 'hh_income'})  # Rename the variable
     df_hh_income['hh_income'] = df_hh_income['hh_income'] / 12.0  # Transform from yearly to monthly income
-    df_hh_income['hh_income'] = np.select(condlist=[df_hh_income['hh_income'] < 2000,
-                                                    df_hh_income['hh_income'].between(2000, 4000),
-                                                    df_hh_income['hh_income'].between(4001, 6000),
-                                                    df_hh_income['hh_income'].between(6001, 8000),
-                                                    df_hh_income['hh_income'].between(8001, 10000),
-                                                    df_hh_income['hh_income'].between(10001, 12000),
-                                                    df_hh_income['hh_income'].between(12001, 14000),
-                                                    df_hh_income['hh_income'].between(14001, 16000),
-                                                    16000 < df_hh_income['hh_income']],
-                                          choicelist=[1, 2, 3, 4, 5, 6, 7, 8, 9],
-                                          default=-98)  # Transform CHF to MTMC categories
+    df_hh_income['hh_income_category'] = np.select(condlist=[df_hh_income['hh_income'] < 2000,
+                                                             df_hh_income['hh_income'].between(2000, 4000),
+                                                             df_hh_income['hh_income'].between(4001, 6000),
+                                                             df_hh_income['hh_income'].between(6001, 8000),
+                                                             df_hh_income['hh_income'].between(8001, 10000),
+                                                             df_hh_income['hh_income'].between(10001, 12000),
+                                                             df_hh_income['hh_income'].between(12001, 14000),
+                                                             df_hh_income['hh_income'].between(14001, 16000),
+                                                             16000 < df_hh_income['hh_income']],
+                                                   choicelist=[1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                                   default=-98)  # Transform CHF to MTMC categories
     df_persons = pd.merge(df_persons, df_hh_income, on='household_id', how='left')  # Add the result to df_persons
     del df_persons['income']
     return df_persons
+
+
+def add_public_transport_connection_quality_work(df_businesses):
+    """ Add connection quality of public transport from coordinates
+    :param df_businesses: Contains the businesses from the SynPop, incl. coordinates
+    :return: df_businesses: Contains the businesses from the SynPop, including a column containing the public
+    transport connection quality
+    - A = very good, coded as 1
+    - B = good, coded as 2
+    - C = medium, coded as 3
+    - D = low, coded as 4
+    - 5 = marginal or no public transport connection
+    """
+    # Read the shape file containing the connection quality
+    connection_quality_folder_path = Path('../data/input/OeV_Gueteklassen/Fahrplanperiode_17_18/')
+    df_connection_quality = geopandas.read_file(connection_quality_folder_path / 'OeV_Gueteklassen_ARE.shp')
+    df_connection_quality.to_crs(epsg=2056, inplace=True)  # Change the projection
+    # print(df_persons[['public_transport_connection_quality_ARE_home', 'xcoord_home', 'ycoord_home']])
+    geodf_businesses = geopandas.GeoDataFrame(df_businesses,
+                                              geometry=geopandas.points_from_xy(df_businesses.xcoord_work,
+                                                                                df_businesses.ycoord_work),
+                                              crs='epsg:2056')
+    geodf_businesses = geopandas.sjoin(geodf_businesses, df_connection_quality[['KLASSE', 'geometry']],
+                                      how='left', op='intersects')
+    geodf_businesses['KLASSE'] = geodf_businesses['KLASSE'].map({'A': 1,
+                                                                 'B': 2,
+                                                                 'C': 3,
+                                                                 'D': 4})
+    geodf_businesses['KLASSE'].fillna(5, inplace=True)
+    # Rename the column with the public transport connection quality
+    geodf_businesses.rename(columns={'KLASSE': 'public_transport_connection_quality_ARE_work'}, inplace=True)
+    # base = df_connection_quality.plot()
+    # geodf_businesses[geodf_businesses['public_transport_connection_quality_ARE_work'] == 1].head(n=5000).plot(ax=base,
+    #                                                                   marker='o',
+    #                                                                   color='red',
+    #                                                                   markersize=5)
+    # plt.show()
+    geodf_businesses.drop(['index_right', 'geometry'], axis=1, inplace=True)
+    return geodf_businesses
 
 
 def add_public_transport_connection_quality(df_persons, geodf_household):
@@ -142,12 +193,30 @@ def add_public_transport_connection_quality(df_persons, geodf_household):
     geodf_household = geopandas.sjoin(geodf_household, df_connection_quality[['KLASSE', 'geometry']],
                                       how='left', op='intersects')
     geodf_household['KLASSE'] = geodf_household['KLASSE'].map({'A': 1,
-                                                           'B': 2,
-                                                           'C': 3,
-                                                           'D': 4})
+                                                               'B': 2,
+                                                               'C': 3,
+                                                               'D': 4})
     geodf_household['KLASSE'].fillna('5', inplace=True)
     # Rename the column with the public transport connection quality
-    geodf_household.rename(columns={'KLASSE': 'public_transport_connection_quality_ARE'}, inplace=True)
+    geodf_household.rename(columns={'KLASSE': 'public_transport_connection_quality_ARE_home'}, inplace=True)
     df_persons = pd.merge(df_persons, geodf_household, on='household_id', how='left')
     del df_persons['index_right']
     return df_persons
+
+
+def add_spatial_typology(df_zp):
+    ''' Add the data about the spatial typology of the home address (in particular the home commune) '''
+    path_to_typology = Path('../data/input/StadtLandTypologie/2015/Raumgliederungen.xlsx')
+    df_typology = pd.read_excel(path_to_typology, sheet_name='Daten',
+                                skiprows=[0, 2],  # Removes the 1st row, with information, and the 2nd, with links
+                                usecols='A,G')  # Selects only the BFS commune number and the column with the typology
+    df_zp = pd.merge(df_zp, df_typology, left_on='W_BFS', right_on='BFS Gde-nummer', how='left')
+    df_zp.drop('BFS Gde-nummer', axis=1, inplace=True)
+    df_zp = df_zp.rename(columns={'Stadt/Land-Typologie': 'urban_typology_home'})
+
+    ''' Add the data about the spatial typology of the work address (in particular the work commune) '''
+    df_zp = pd.merge(df_zp, df_typology, left_on='A_BFS', right_on='BFS Gde-nummer', how='left')
+    df_zp.drop('BFS Gde-nummer', axis=1, inplace=True)
+    df_zp = df_zp.rename(columns={'Stadt/Land-Typologie': 'urban_typology_work'})
+    df_zp.urban_typology_work.fillna(-99, inplace=True)
+    return df_zp
