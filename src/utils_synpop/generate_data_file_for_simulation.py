1d2447abfd13e6720999e99ff8b9a4da5e5b8719
antonindanalet <antonin.danalet>
2020-12-31 18:28:41 +0100

    Generate data files for 2030, 2040 and 2050 in Biogeme format.

--- src/utils_synpop/generate_data_file_for_simulation.py
+++ src/utils_synpop/generate_data_file_for_simulation.py
@@ -7,17 +7,17 @@ import matplotlib.pyplot as plt
 import geopandas
 
 
-def generate_data_file_for_simulation():
+def generate_data_file_for_simulation(year):
     """ Generates the data file for applying the model to the synthetic population (SynPop).
     In particular, it changes the format of the SynPop and adds some geospatial information,
     the connection quality of public transport where the person lives (in German: OeV-Gueteklassen, see
     https://www.are.admin.ch/are/de/home/mobilitaet/grundlagen-und-daten/verkehrserschliessung-in-der-schweiz.html
     :return: Nothing, but saves a CSV file 'persons_from_SynPop2017.csv' in data/output/data/application_to_synpop/
     """
-    df_persons = get_persons_from_synthetic_population()
+    df_persons = get_persons_from_synthetic_population(year)
     nb_of_persons_before = len(df_persons)
-    df_persons = add_information_about_households_from_synthetic_population(df_persons)
-    df_persons = add_information_about_businesses_from_synthetic_population(df_persons)
+    df_persons = add_information_about_households_from_synthetic_population(df_persons, year)
+    df_persons = add_information_about_businesses_from_synthetic_population(df_persons, year)
     ''' Test that the number of people are still the same '''
     nb_of_persons_after = len(df_persons)
     if nb_of_persons_after != nb_of_persons_before:
@@ -27,18 +27,43 @@ def generate_data_file_for_simulation():
         if df_persons[column].isna().any():
             print('There are NA values in column', column)
     ''' Save the file '''
-    output_directory = Path('../data/output/data/validation_with_Synpop/')
-    data_file_name = 'persons_from_SynPop2017.csv'
+    if year == 2017:
+        output_directory = Path('../data/output/data/validation_with_Synpop/')
+        data_file_name = 'persons_from_SynPop2017.csv'
+    elif year == 2030:
+        output_directory = Path('../data/output/data/application_to_SynPop_2030/')
+        data_file_name = 'persons_from_SynPop2030.csv'
+    elif year == 2040:
+        output_directory = Path('../data/output/data/application_to_SynPop_2040/')
+        data_file_name = 'persons_from_SynPop2040.csv'
+    elif year == 2050:
+        output_directory = Path('../data/output/data/application_to_SynPop_2050/')
+        data_file_name = 'persons_from_SynPop2050.csv'
+    else:
+        raise Exception('Reference year for synthetic population not well defined (' + str(year) + ')')
     df_persons.to_csv(output_directory / data_file_name, sep=';', index=False)
 
 
-def add_information_about_businesses_from_synthetic_population(df_persons):
+def add_information_about_businesses_from_synthetic_population(df_persons, year):
     ''' Read the companies from the synthetic population '''
-    synpop_folder_path = Path('../data/input/SynPop/2017/')
+    if year == 2017:
+        synpop_folder_path = Path('../data/input/SynPop/2017/')
+        synpop_business_file_name = 'businesses.csv'
+    elif year == 2030:
+        synpop_folder_path = Path('../data/input/SynPop/2030/')
+        synpop_business_file_name = 'businesses_overview_1_2030.csv'
+    elif year == 2040:
+        synpop_folder_path = Path('../data/input/SynPop/2040/')
+        synpop_business_file_name = 'businesses_overview_1_2040.csv'
+    elif year == 2050:
+        synpop_folder_path = Path('../data/input/SynPop/2050/')
+        synpop_business_file_name = 'businesses_overview_1_2050.csv'
+    else:
+        raise Exception('Reference year of the synthetic population not well defined for "businesses"')
     selected_columns = ['business_id',
                         'xcoord',  # Swiss coordinates CH1903+ / LV95 (ex: 2566304.0)
                         'ycoord']  # Swiss coordinates CH1903+ / LV95 (ex: 1181454.0)
-    with open(synpop_folder_path / 'businesses.csv', 'r') as businesses_file:
+    with open(synpop_folder_path / synpop_business_file_name, 'r') as businesses_file:
         df_businesses = pd.read_csv(businesses_file, sep=';', usecols=selected_columns)
     df_businesses.rename(columns={'xcoord': 'xcoord_work',
                                   'ycoord': 'ycoord_work'}, inplace=True)
@@ -73,13 +98,26 @@ def add_home_work_distance(df_persons):
     return df_persons
 
 
-def add_information_about_households_from_synthetic_population(df_persons):
+def add_information_about_households_from_synthetic_population(df_persons, year):
     ''' Read the households from the synthetic population '''
-    synpop_folder_path = Path('../data/input/SynPop/2017/')
+    if year == 2017:
+        synpop_folder_path = Path('../data/input/SynPop/2017/')
+        synpop_households_file_name = 'households.csv'
+    elif year == 2030:
+        synpop_folder_path = Path('../data/input/SynPop/2030/')
+        synpop_households_file_name = 'households_overview_1_2030.csv'
+    elif year == 2040:
+        synpop_folder_path = Path('../data/input/SynPop/2040/')
+        synpop_households_file_name = 'households_overview_1_2040.csv'
+    elif year == 2050:
+        synpop_folder_path = Path('../data/input/SynPop/2050/')
+        synpop_households_file_name = 'households_overview_1_2050.csv'
+    else:
+        raise Exception('Reference year of the synthetic population not well defined for "households"')
     selected_columns = ['household_id',
                         'xcoord',  # Swiss coordinates CH1903+ / LV95 (ex: 2566304.0)
                         'ycoord']  # Swiss coordinates CH1903+ / LV95 (ex: 1181454.0)
-    with open(synpop_folder_path / 'households.csv', 'r') as households_file:
+    with open(synpop_folder_path / synpop_households_file_name, 'r') as households_file:
         df_households = pd.read_csv(households_file, sep=';', usecols=selected_columns)
     df_households.rename(columns={'xcoord': 'xcoord_home',
                                   'ycoord': 'ycoord_home'}, inplace=True)
@@ -95,22 +133,35 @@ def add_information_about_households_from_synthetic_population(df_persons):
     return df_persons
 
 
-def get_persons_from_synthetic_population():
+def get_persons_from_synthetic_population(year):
     ''' Read the persons from the synthetic population '''
-    synpop_folder_path = Path('../data/input/SynPop/2017/')
+    if year == 2017:
+        synpop_folder_path = Path('../data/input/SynPop/2017/')
+        synpop_persons_file_name = 'persons.csv'
+    elif year == 2030:
+        synpop_folder_path = Path('../data/input/SynPop/2030/')
+        synpop_persons_file_name = 'persons_overview_1_2030.csv'
+    elif year == 2040:
+        synpop_folder_path = Path('../data/input/SynPop/2040/')
+        synpop_persons_file_name = 'persons_overview_1_2040.csv'
+    elif year == 2050:
+        synpop_folder_path = Path('../data/input/SynPop/2050/')
+        synpop_persons_file_name = 'persons_overview_1_2050.csv'
+    else:
+        raise Exception('Reference year of the synthetic population not well defined for "persons"')
     selected_columns = ['position_in_bus',  # information about work THERE IS A CORRECTED VERSION OF THE VARIABLE, type_5 !!!
                         'type_2',  # level of employment (in %) THERE IS A CORRECTED VERSION OF THE VARIABLE, type_6 !!!!
                         'person_id', 'household_id', 'sex', 'education', 'income', 'language', 'nation',
                         'dbirth',  # date of birth
                         'business_id',
                         'type_1']
-    with open(synpop_folder_path / 'persons.csv', 'r') as persons_file:
+    with open(synpop_folder_path / synpop_persons_file_name, 'r') as persons_file:
         df_persons = pd.read_csv(persons_file, sep=';', usecols=selected_columns)
     ''' Transform the data structure '''
     # Replace NA values by -99 for position_in_bus
     df_persons['position_in_bus'] = df_persons['position_in_bus'].fillna(-99)
-    # Transform the 'date of birth' variable to an 'age' variable (reference year: 2017)
-    df_persons['age'] = 2017 - df_persons['dbirth'].str.split('-', expand=True)[2].astype(int)
+    # Transform the 'date of birth' variable to an 'age' variable (reference year: 2017 or 2050)
+    df_persons['age'] = year - df_persons['dbirth'].str.split('-', expand=True)[2].astype(int)
     del df_persons['dbirth']
     # Transform the variable about employment to a variable about being employed
     df_persons['employed'] = df_persons['position_in_bus'].apply(lambda x:
@@ -122,17 +173,17 @@ def get_persons_from_synthetic_population():
     df_hh_income = df_persons[['household_id', 'income']].groupby(['household_id']).sum()  # Aggregate over people
     df_hh_income = df_hh_income.rename(columns={'income': 'hh_income'})  # Rename the variable
     df_hh_income['hh_income'] = df_hh_income['hh_income'] / 12.0  # Transform from yearly to monthly income
-    df_hh_income['hh_income_category'] = np.select(condlist=[df_hh_income['hh_income'] < 2000,
-                                                             df_hh_income['hh_income'].between(2000, 4000),
-                                                             df_hh_income['hh_income'].between(4001, 6000),
-                                                             df_hh_income['hh_income'].between(6001, 8000),
-                                                             df_hh_income['hh_income'].between(8001, 10000),
-                                                             df_hh_income['hh_income'].between(10001, 12000),
-                                                             df_hh_income['hh_income'].between(12001, 14000),
-                                                             df_hh_income['hh_income'].between(14001, 16000),
-                                                             16000 < df_hh_income['hh_income']],
-                                                   choicelist=[1, 2, 3, 4, 5, 6, 7, 8, 9],
-                                                   default=-98)  # Transform CHF to MTMC categories
+    # df_hh_income['hh_income_category'] = np.select(condlist=[df_hh_income['hh_income'] < 2000,
+    #                                                          df_hh_income['hh_income'].between(2000, 4000),
+    #                                                          df_hh_income['hh_income'].between(4001, 6000),
+    #                                                          df_hh_income['hh_income'].between(6001, 8000),
+    #                                                          df_hh_income['hh_income'].between(8001, 10000),
+    #                                                          df_hh_income['hh_income'].between(10001, 12000),
+    #                                                          df_hh_income['hh_income'].between(12001, 14000),
+    #                                                          df_hh_income['hh_income'].between(14001, 16000),
+    #                                                          16000 < df_hh_income['hh_income']],
+    #                                                choicelist=[1, 2, 3, 4, 5, 6, 7, 8, 9],
+    #                                                default=-98)  # Transform CHF to MTMC categories
     df_persons = pd.merge(df_persons, df_hh_income, on='household_id', how='left')  # Add the result to df_persons
     del df_persons['income']
     return df_persons
